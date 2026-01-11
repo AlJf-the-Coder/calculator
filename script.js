54fa39f4bfc70e0359fc4b1763b9fe5b3c59802c
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-11 19:15:46 +0800

    feat: add input parsing

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operand1;
+let operand2;
+let operator;
+
 const display = document.querySelector('#display')
 const digitButtons = document.querySelectorAll('.digits button');
 const operatorButtons = document.querySelectorAll('.operators button');
@@ -10,7 +14,31 @@ digitButtons.forEach(btn => btn.addEventListener('click',
 
 operatorButtons.forEach(btn => btn.addEventListener('click', 
     () => {
-        display.textContent += btn.textContent;
+        if (operand1) {
+            const lastOpIndex = display.textContent.lastIndexOf(operator)
+            // replace operator if last input is also operator
+            if (lastOpIndex == display.textContent.length - 1){
+                operator = btn.textContent;
+                return;
+            }
+            operand2 = display.textContent.slice(lastOpIndex + 1)
+            // validate operand
+            operand2 = +operand2;
+
+            const result = operate(operand1, operator, operand2);
+            operand1 = result;
+            display.textContent = result.toString();
+        } else {
+            // case where operator is input first
+            // ...
+            operand1 = display.textContent;
+            //validate operand
+            operand1 = +operand1;
+        }
+        if (operator != '='){
+            operator = btn.textContent;
+        }
+        display.textContent += operator;
     }
 ))
 
@@ -29,10 +57,6 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
-
-let operand1;
-let operand2;
-let operator;
 
 function operate(a, op, b){
     switch (op){

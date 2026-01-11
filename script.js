dc4edfbb14db307521b3ffad5aecf45d6a9fd555
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-11 19:57:47 +0800

    refactor: use arrow functions for operations, and object instead of switch statement

--- script.js
+++ script.js
@@ -43,33 +43,13 @@ operatorButtons.forEach(btn => btn.addEventListener('click',
     }
 ))
 
-function add(a, b){
-    return a + b;
-}
-
-function subtract(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
+const operations = {
+    '+': (a, b) => a + b,
+    '−': (a, b) => a - b,
+    '×': (a, b) => a * b,
+    '÷': (a, b) => a / b,
 }
 
 function operate(a, op, b){
-    switch (op){
-        case '+':
-            return add(a, b);
-        case '−':
-            return subtract(a, b);
-        case '×':
-            return multiply(a, b);
-        case '÷':
-            return divide(a, b);
-        default:
-            return;
-    }
+    return operations[op](a, b);
 }

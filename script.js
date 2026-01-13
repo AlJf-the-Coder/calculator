72fa2184d27a589bbf6225a64b3ec1652d8f93f0
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-13 19:02:31 +0800

    refactor: store partial operands as array of characters

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let operand1 = null;
-let operand2 = null; 
+let operand1 = ['0'];
+let operand2 = []; 
 let operator = null;
 
 const display = document.querySelector('#display')
@@ -9,9 +9,9 @@ const clearButton = document.querySelector('.functions #clear')
 
 clearButton.addEventListener('click', () => {
     display.textContent = '0';
-    operand1 = null;
+    operand1 = ['0'];
+    operand2 = [];
     operator = null;
-    operand2 = null;
 })
 
 digitButtons.forEach(btn => btn.addEventListener('click', 
@@ -20,6 +20,7 @@ digitButtons.forEach(btn => btn.addEventListener('click',
         const hasParsedOperand1 = typeof operand1 == 'number';
         if (!hasParsedOperand1 && display.textContent == '0'){
             display.textContent = digit;
+            operand1 = [digit];
             return;
         }
 
@@ -29,16 +30,20 @@ digitButtons.forEach(btn => btn.addEventListener('click',
             if (hasCompletedCalculation){
                 //reset calculator
                 display.textContent = digit;
-                operand1 = null;
+                operand1 = [digit];
+                operand2 = [];
                 operator = null;
-                operand2 = null;
-                return;
-            } else if (display.textContent.slice(lastOpIndex + 1) == '0') {
+            } else if (operand2.length == 1 && operand2[0] == '0') {
                 display.textContent = display.textContent.slice(0, lastOpIndex + 1) + digit;
-                return;
+                operand2 = [digit];
+            } else {
+                display.textContent += digit;
+                operand2.push(digit)
             }
+        } else {
+            display.textContent += digit;
+            operand1.push(digit)
         }
-        display.textContent += digit;
     })
 )
 
@@ -60,20 +65,19 @@ operatorButtons.forEach(btn => btn.addEventListener('click',
             if (lastOpIndex == display.textContent.length - 1 && currentOp == '='){
                 operand2 = operand1;
             } else if (!hasCompletedCalculation){
-                operand2 = display.textContent.slice(lastOpIndex + 1)
-                operand2 = +operand2;
+                operand2 = +operand2.join('');
             } 
 
             if (!hasCompletedCalculation || operator != null && currentOp == '='){
                 const result = operate(operand1, operator, operand2);
                 operand1 = result;
                 display.textContent = result.toString();
+                operand2 = (currentOp != '=') ? [] : operand2;
             } else {
-                operand2 = null;
+                operand2 = [];
             }
         } else {
-            operand1 = display.textContent;
-            operand1 = +operand1;
+            operand1 = +operand1.join('');
         }
 
         if (currentOp != '='){

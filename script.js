49a5fcb52d4f81118e9137228ae6894fb327596f
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-18 16:54:28 +0800

    feat: add operator keys and enter key support

--- script.js
+++ script.js
@@ -26,17 +26,26 @@ const backspaceButton = document.querySelector('button#backspace');
 
 document.addEventListener('keydown', (e)=> {
     const key = e.key;
-    const digits = "0123456789";
+    console.log(key);
+    const digits = '0123456789';
     if (digits.includes(key)){
         digitHandler(key);
         return;
     }
     switch (key){
-        case "Backspace":
+        case 'Backspace':
             backspaceHandler();
             break;
-        case ".":
+        case '.':
             dotHandler();
+            break;
+        case '=':
+        case 'Enter':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            operatorHandler(key);
             break;
     }
 })
@@ -146,6 +155,100 @@ function digitHandler(digit){
     }
 }
 
+function operatorHandler(currentOp){
+    switch (state){
+        case states.OPERAND1:
+            if (currentOp == '=' || currentOp == 'Enter'){
+                operand1 = +operand1.join('');
+                let result;
+                if (operator && typeof operand2 == "number"){
+                    result = operate(operand1, operator, operand2);
+                    displayCalculation.textContent = `${operand1}${operator}${operand2}=`
+                    state = states.DONE;
+                } else {
+                    result = operand1;
+                    displayCalculation.textContent = `${operand1}=`
+                    state = states.SELF;
+                }
+                displayResult.textContent = result.toString();
+                operand1 = result;
+                dotButton.disabled = false;
+            } else {
+                operand1 = +operand1.join('');
+                displayCalculation.textContent += `${operand1}${currentOp}`;
+                operand2 = [];
+                operator = currentOp;
+                state = states.OPERATOR;
+                dotButton.disabled = false;
+            }
+            break;
+        case states.OPERATOR:
+            if (currentOp == "=" || currentOp == 'Enter'){
+                operand2 = operand1;
+                const result = operate(operand1, operator, operand2);
+                if (typeof result == "string"){
+                    displayResult.textContent = result;
+                    displayCalculation.textContent = '';
+                    state = states.ERROR;
+                } else {
+                    displayCalculation.textContent = `${operand1}${operator}${operand2}=`
+                    displayResult.textContent = roundDecimals(result);
+                    operand1 = +displayResult.textContent;
+                    state = states.DONE;
+                }
+                dotButton.disabled = false;
+            } else {
+                displayCalculation.textContent = displayCalculation.textContent.slice(0, -1) + currentOp;
+                operator = currentOp;
+            }
+            break;
+        case states.OPERAND2:
+            operand2 = +operand2.join('');
+            const result = operate(operand1, operator, operand2);
+            dotButton.disabled = false;
+            if (typeof result == "string"){
+                displayResult.textContent = result;
+                displayCalculation.textContent = '';
+                state = states.ERROR;
+                return;
+            }
+            displayResult.textContent = roundDecimals(result);
+            if (currentOp == '=' || currentOp == 'Enter'){
+                displayCalculation.textContent = `${operand1}${operator}${operand2}=`;
+                state = states.DONE;
+            } else {
+                displayCalculation.textContent = `${result}${currentOp}`;
+                operator = currentOp;
+                operand2 = []; 
+                state = states.OPERATOR;
+            }
+            operand1 = +displayResult.textContent;
+            break;
+        case states.SELF:
+            if (currentOp == '=' || currentOp == 'Enter') return;
+            displayCalculation.textContent = `${operand1}${currentOp}`;
+            operator = currentOp;
+            operand2 = []; 
+            state = states.OPERATOR;
+            dotButton.disabled = false;
+            break;
+        case states.DONE:
+            if (currentOp == '=' || currentOp == 'Enter'){
+                const result = operate(operand1, operator, operand2);
+                displayCalculation.textContent = `${operand1}${operator}${operand2}=`
+                displayResult.textContent = roundDecimals(result);
+                operand1 = +displayResult.textContent;
+            } else {
+                displayCalculation.textContent = `${operand1}${currentOp}`;
+                operator = currentOp;
+                operand2 = []; 
+                state = states.OPERATOR;
+                dotButton.disabled = false;
+            }
+            break;
+    }
+}
+
 backspaceButton.addEventListener('click', backspaceHandler);
 
 clearButton.addEventListener('click', () => {
@@ -165,101 +268,8 @@ digitButtons.forEach(btn => btn.addEventListener('click',
 )
 
 operatorButtons.forEach(btn => btn.addEventListener('click', 
-    () => {
-        const currentOp = btn.textContent;
-        switch (state){
-            case states.OPERAND1:
-                if (currentOp == '='){
-                    operand1 = +operand1.join('');
-                    let result;
-                    if (operator && typeof operand2 == "number"){
-                        result = operate(operand1, operator, operand2);
-                        displayCalculation.textContent = `${operand1}${operator}${operand2}=`
-                        state = states.DONE;
-                    } else {
-                        result = operand1;
-                        displayCalculation.textContent = `${operand1}=`
-                        state = states.SELF;
-                    }
-                    displayResult.textContent = result.toString();
-                    operand1 = result;
-                    dotButton.disabled = false;
-                } else {
-                    operand1 = +operand1.join('');
-                    displayCalculation.textContent += `${operand1}${currentOp}`;
-                    operand2 = [];
-                    operator = currentOp;
-                    state = states.OPERATOR;
-                    dotButton.disabled = false;
-                }
-                break;
-            case states.OPERATOR:
-                if (currentOp == "="){
-                    operand2 = operand1;
-                    const result = operate(operand1, operator, operand2);
-                    if (typeof result == "string"){
-                        displayResult.textContent = result;
-                        displayCalculation.textContent = '';
-                        state = states.ERROR;
-                    } else {
-                        displayCalculation.textContent = `${operand1}${operator}${operand2}=`
-                        displayResult.textContent = roundDecimals(result);
-                        operand1 = +displayResult.textContent;
-                        state = states.DONE;
-                    }
-                    dotButton.disabled = false;
-                } else {
-                    displayCalculation.textContent = displayCalculation.textContent.slice(0, -1) + currentOp;
-                    operator = currentOp;
-                }
-                break;
-            case states.OPERAND2:
-                operand2 = +operand2.join('');
-                const result = operate(operand1, operator, operand2);
-                dotButton.disabled = false;
-                if (typeof result == "string"){
-                    displayResult.textContent = result;
-                    displayCalculation.textContent = '';
-                    state = states.ERROR;
-                    return;
-                }
-                displayResult.textContent = roundDecimals(result);
-                if (currentOp == '='){
-                    displayCalculation.textContent = `${operand1}${operator}${operand2}=`;
-                    state = states.DONE;
-                } else {
-                    displayCalculation.textContent = `${result}${currentOp}`;
-                    operator = currentOp;
-                    operand2 = []; 
-                    state = states.OPERATOR;
-                }
-                operand1 = +displayResult.textContent;
-                break;
-            case states.SELF:
-                if (currentOp == '=') return;
-                displayCalculation.textContent = `${operand1}${currentOp}`;
-                operator = currentOp;
-                operand2 = []; 
-                state = states.OPERATOR;
-                dotButton.disabled = false;
-                break;
-            case states.DONE:
-                if (currentOp == '='){
-                    const result = operate(operand1, operator, operand2);
-                    displayCalculation.textContent = `${operand1}${operator}${operand2}=`
-                    displayResult.textContent = roundDecimals(result);
-                    operand1 = +displayResult.textContent;
-                } else {
-                    displayCalculation.textContent = `${operand1}${currentOp}`;
-                    operator = currentOp;
-                    operand2 = []; 
-                    state = states.OPERATOR;
-                    dotButton.disabled = false;
-                }
-                break;
-        }
-    }
-))
+    () => operatorHandler(btn.textContent))
+)
 
 function roundDecimals(num){
     const numString = num.toString();
@@ -276,7 +286,10 @@ function roundDecimals(num){
 const operations = {
     '+': (a, b) => a + b,
     '−': (a, b) => a - b,
+    '-': (a, b) => a - b,
     '×': (a, b) => a * b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
     '÷': (a, b) => (b != 0) ? a / b : "Oops! You divided by zero!",
 }
 
